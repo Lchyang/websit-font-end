0228e04a5a79d712a5a20f2c5eac3c5e507ba645
QJ李春杨 <18831627116@163.com>
2021-06-07 14:35:13 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <home></home>
+</template>
+<script setup>
+import Home from './views/Home.vue'
+</script>
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="test">Hello world</div>
+</template>
+<script>
+</script>
+<style lang="scss">
+    .test{
+        color: red;
+        font-size: 32px;
+    }
+</style>
